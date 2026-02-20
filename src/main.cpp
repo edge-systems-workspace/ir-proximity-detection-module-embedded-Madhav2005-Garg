9694ad6999e6fda4115d14abc8b37d373fcc3dd6
Madhav2005-Garg <madhavgarg545@gmail.com>
2026-02-20 22:09:12 +0530

    initialize GPIO and serial telemetry

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@
 
 int sensorState = 0;
 
+// ... previous code ...
+
 void setup() {
+    Serial.begin(9600);
+
+    // Configure IR sensor pin as input
+    pinMode(IR_PIN, INPUT);
+
+    Serial.println("IR Obstacle Detection System - Initialized");
+    delay(1000);
 }
 
 void loop() {
